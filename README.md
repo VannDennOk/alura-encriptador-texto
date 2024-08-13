ce9440d40addb0107d73df9f246f1c144168447c
Vanina Denegri <denegri.vanina@gmail.com>
2024-08-13 02:56:26 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,81 @@
+<h1>ENCRIPTADOR DE TEXTO :mag:</h1>
+
+<p align="center">
+    <img src="./assets/Portada.png" alt="vista del encriptador" width="600">
+</p>
+<br>
+<p align="left">
+    <img src="https://img.shields.io/badge/STATUS-EN%20DESAROLLO-green">
+</p>
+
+
+<h2>Descripción del proyecto :bulb:</h2>
+<p>
+    Encriptador de texto realizado para el Challenge ONE Front End de Alura-Latam.
+    <br>Esta página web permite encriptar y desencriptar un texto, además de copiarlo al portapapeles.
+</p>
+
+<h3>Funcionalidad :wrench:</h3>
+<p>
+    La encriptación funciona de ida y vuelta usando las claves:
+    <br>
+    <br>:small_blue_diamond: La letra "a" es convertida para "al"
+    <br>:small_blue_diamond: La letra "e" es convertida para "enter"
+    <br>:small_blue_diamond: La letra "i" es convertida para "imes"
+    <br>:small_blue_diamond: La letra "o" es convertida para "ober"
+    <br>:small_blue_diamond: La letra "u" es convertida para "ufat"
+</p>
+
+<h3>Requisitos :clipboard:</h3>
+<p>
+    <br>:heavy_check_mark: Debe funcionar solo con letras minúsculas
+    <br>:heavy_check_mark: No deben ser utilizados letras con acentos ni caracteres especiales.
+    <br>:heavy_check_mark: La página debe tener campos para la inserción del texto que será encriptado o desencriptado.
+    <br>:heavy_check_mark: El usuario debe poder escoger entre las dos opciones: encriptar o desencriptar.
+    <br>:heavy_check_mark: El resultado debe ser mostrado en la pantalla.
+</p>
+
+<h3>Ejemplo :thumbsup:</h3>      
+<p align="left">
+    <img src="https://img.shields.io/badge/gato_=>_galtober_<=>_galtober_=>_gato-gray?style=flat">
+</p>
+
+<h3>Extras :star:</h3>
+<p>
+    Un botón que copie el texto encriptado/desencriptado para la sección de transferencia, o sea que tenga la misma funcionalidad del ctrl+C o de la opción "copiar" del menú de las aplicaciones.
+</p>
+
+<h2>Acceso al proyecto :link:</h2>
+<p> 
+    Versión v.1:
+<br> 
+    &#9670;
+    <a href="https://github.com/VannDennOk/encriptador-texto-alura"><img alt="Static Badge" src="https://img.shields.io/badge/GitHub_del_proyecto_v1-DarkViolet"></a> 
+    &#9670; 
+    <a href="https://vanndennok.github.io/encriptador-texto-alura"><img alt="Static Badge" src="https://img.shields.io/badge/GitHub_Page_v1-DarkViolet"></a> 
+    &#9670;
+</p>
+
+<h2>Autoría :woman:</h2>
+<p>
+    El proyecto fue desarrollado por Vanina Denegri en base a los diseños provistos por Alura-Latam.
+<br>
+    &#9632;
+    <a href="https://github.com/VannDennOk/"><img alt="Static Badge" src="https://img.shields.io/badge/Mi_perfil_de_GitHub-blue"></a> 
+    &#9632; 
+    <a href="https://www.linkedin.com/in/vaninadenegri/"><img alt="Static Badge" src="https://img.shields.io/badge/Mi_perfil_de_LinkedIn-DarkBlue"></a> 
+    &#9632;
+</p>
+
+<h2>Reconocimientos :crown:</h2>
+<p>
+    Links útiles y utilizados para realizar este README:
+<br>
+    &#9679;
+    <a href="https://gist.github.com/rxaviers/7360908"><img alt="Static Badge" src="https://img.shields.io/badge/Lista_de_emojis_para_GitHub-DarkSlateGray"></a>
+    &#9679;
+    <a href="https://shields.io"><img alt="Static Badge" src="https://img.shields.io/badge/Img Shields-DarkSlateGray"></a>
+    &#9679;
+    <a href="https://github.com/othneildrew/Best-README-Template/tree/master"><img alt="Static Badge" src="https://img.shields.io/badge/Ejemplo_Modelo_de_ReadMe-DarkSlateGray "></a>
+    &#9679; 
+</p>
---
+++ app.js
@@ -0,0 +1,157 @@
+// VARIABLES
+const caracteresEsp = /[\u0022-\u002B\u002F\u0030-\u0039\u003C-\u003E\u0040-\u0060\u007B-\u007E\u00A2-\u00BF\u00C0-\u00FF\u0100-\u017F\u0180-\u024F]/g;
+          // caracteresEsp: excluye elementos del latín básico, latín suplemento, latín extendido A y B (uso UNICODE). Sí se permite: !¡,.:;-¿?
+
+// ESTADO DE LA PAGINA AL INICIO
+function condicionesIniciales(){
+  document.getElementById("mensajeInicial").value = "";
+  document.getElementById("ocultar").style.display= "block";
+  document.getElementById("mensajeFinal").style.display= "none";
+  document.getElementById("copiar").style.display= "none";
+}
+
+// FUNCION PARA ENCRIPTAR TEXTO
+function encriptar(){
+  let texto = document.getElementById("mensajeInicial").value;
+  let textoCifrado = texto
+    .replace(/a/gi, "al")
+    .replace(/e/gi, "enter")
+    .replace(/i/gi, "imes")
+    .replace(/o/gi, "ober")
+    .replace(/u/gi, "ufat");
+
+    if (texto.match(caracteresEsp)){     //Valida que no haya caracteres especiales
+      mensajeError();
+    } else if (texto.length != "" && !texto.match(caracteresEsp)) {     //solo encripta si no hay caracteres especiales
+      document.getElementById("mensajeFinal").value = textoCifrado;
+      cambioDisplay();
+      } else {
+        mensajeCampoVacio();
+      }
+  }
+
+// FUNCION PARA DESENCRIPTAR TEXTO
+function desencriptar(){
+  let textoCifrado = document.getElementById("mensajeInicial").value;
+  let texto = textoCifrado
+    .replace(/al/gi, "a")
+    .replace(/enter/gi, "e")
+    .replace(/imes/gi, "i")
+    .replace(/ober/gi, "o")
+    .replace(/ufat/gi, "u");
+
+    if (textoCifrado.match(caracteresEsp)){     //Valida que no haya caracteres especiales
+      mensajeError();
+    } else if (textoCifrado.length != "" && !texto.match(caracteresEsp)) {     //solo desencripta si no hay caracteres especiales
+      document.getElementById("mensajeFinal").value = texto;
+      cambioDisplay();
+      } else {
+        mensajeCampoVacio();
+      }
+  }
+
+// OCULTA LA IMAGEN Y MUESTRA EL TEXTO ENCRIPTADO/DESENCRIPTADO
+function cambioDisplay(){
+  document.getElementById("mensajeInicial").value = "";
+  document.getElementById("ocultar").style.display= "none";
+  document.getElementById("mensajeFinal").style.display= "block";
+  document.getElementById("copiar").style.display= "block";
+}
+
+// COPIA EL TEXTO ENCRIPTADO/DESENCRIPTADO EN EL PORTAPAPELES
+function copiarTexto(){
+  var resultado = document.getElementById("mensajeFinal").value;
+  if (resultado.length != 0) {
+    navigator.clipboard.writeText(resultado);
+    mensajeExitoCopiar();
+  } else {
+      mensajeCampoVacioCopiar();
+  }
+}
+
+// MENSAJES POPUP
+// Ahora son funcionales pero faltaría revisar cómo acortar el código.
+function mensajeCampoVacio(){
+  var textarea = document.getElementById("mensajeInicial").value;
+  var textarea2 = document.getElementById("mensajeInicial").value;
+  if (textarea === "" || textarea2 === "") {
+    Swal.fire({
+      html: '<p class="popup__texto">Ingresa el texto que desees encriptar o desencriptar</p>',
+      confirmButtonText: 'Aceptar',
+      customClass: {
+        popup:'popup__box',
+        closeButton:'popup__closeButton',
+        confirmButton:'popup__button',
+      },
+      confirmButtonAriaLabel: 'Aceptar',
+      buttonsStyling: false,
+      showCloseButton: true,
+      closeButtonArialLabel: 'cerrar alerta',
+      imageUrl: 'assets/Exclamation.png',
+      imageWidth: '80px',
+      imageAlt: 'icono de exclamación o alerta',
+    });
+  }
+}
+
+function mensajeError(){
+  Swal.fire({
+    html: '<p class="popup__texto">No acepta mayúsculas, acentos o carácteres especiales.<br>Sí se pueden usar signos de puntuación como: , . : ; - ¿? !¡</p>',
+    confirmButtonText: 'Aceptar',
+    customClass: {
+      popup:'popup__box',
+      closeButton:'popup__closeButton',
+      confirmButton:'popup__button',
+    },
+    confirmButtonAriaLabel: 'Aceptar',
+    buttonsStyling: false,
+    showCloseButton: true,
+    closeButtonArialLabel: 'cerrar alerta',
+    imageUrl: 'assets/Exclamation.png',
+    imageWidth: '80px',
+    imageAlt: 'icono de exclamación o alerta',
+  });
+}
+
+function mensajeCampoVacioCopiar(){
+  var textareaValue = document.getElementById("mensajeFinal").value;
+  if (textareaValue.trim() === "") {
+    Swal.fire({
+      html: '<p class="popup__texto">No hay mensaje para copiar</p>',
+      confirmButtonText: 'Aceptar',
+      customClass: {
+        popup:'popup__box',
+        closeButton:'popup__closeButton',
+        confirmButton:'popup__button',
+      },
+      confirmButtonAriaLabel: 'Aceptar',
+      buttonsStyling: false,
+      showCloseButton: true,
+      closeButtonArialLabel: 'cerrar alerta',
+      imageUrl: 'assets/Exclamation.png',
+      imageWidth: '80px',
+      imageAlt: 'icono de exclamación o alerta',
+    });
+  }
+}
+   
+function mensajeExitoCopiar(){
+  Swal.fire({
+    html: '<p class="popup__texto">El mensaje encriptado se ha copiado en el portapapeles</p>',
+    confirmButtonText: 'Aceptar',
+    customClass: {
+      popup:'popup__box',
+      closeButton:'popup__closeButton',
+      confirmButton:'popup__button',
+    },
+    confirmButtonAriaLabel: 'Aceptar',
+    buttonsStyling: false,
+    showCloseButton: true,
+    closeButtonArialLabel: 'cerrar alerta',
+    imageUrl: 'assets/Check.png',
+    imageWidth: '80px',
+    imageAlt: 'icono de check o éxito',
+  });
+}
+
+condicionesIniciales();
